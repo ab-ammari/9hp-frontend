ce5b1572e293aa81a246cc3bce4e410ae54f35bf
ab-ammari <163317985+ab-ammari@users.noreply.github.com>
2025-12-10 15:06:03 +0100

    Add files via upload

--- src/app/Components/widgets/floating-components.module.ts
+++ src/app/Components/widgets/floating-components.module.ts
@@ -3,19 +3,27 @@ import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { FloatingActionButtonComponent } from './floating-action-button/floating-action-button.component';
 import { FloatingPanelComponent } from './floating-panel/floating-panel.component';
+import { FocusedDiagramViewerComponent } from './focused-diagram-viewer/focused-diagram-viewer.component';
+import {FormsModule} from "@angular/forms";
+import { DeleteConfirmationDialogComponent } from './delete-confirmation-dialog/delete-confirmation-dialog.component';
 
 @NgModule({
   declarations: [
     FloatingActionButtonComponent,
-    FloatingPanelComponent
+    FloatingPanelComponent,
+    FocusedDiagramViewerComponent,
+    DeleteConfirmationDialogComponent
   ],
   imports: [
     CommonModule,
-    IonicModule
+    IonicModule,
+    FormsModule
   ],
   exports: [
     FloatingActionButtonComponent,
-    FloatingPanelComponent
+    FloatingPanelComponent,
+    FocusedDiagramViewerComponent,
+    DeleteConfirmationDialogComponent
   ]
 })
 export class FloatingComponentsModule { }
